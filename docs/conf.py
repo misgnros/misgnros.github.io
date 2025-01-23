f094c74c62ea878b1a86332591de0811246968fc
misgnros <igarashi.masanao@gmail.com>
2025-01-23 12:11:05 +0900

    add tags in secondary side bar

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- General ABlog Options ----------------------------------------------------
 
 # A path relative to the configuration directory for blog archive pages.
-# blog_path = 'blog'
+# blog_path = "blog"
 
-# The "title" for the blog, used in active pages.  Default is ``'Blog'``.
+# The "title" for the blog, used in active pages.  Default is ``"Blog"``.
 blog_title = "misgnros.github.io"
 
 # Base URL for the website, required for generating feeds.
@@ -32,7 +32,7 @@ blog_title = "misgnros.github.io"
 # keys should be used in ``post`` directive to refer to the locations.
 # Default is ``{}``.
 # blog_languages = {
-#    'en': ('English', None),
+#    "en": ("English", None),
 # }
 
 
@@ -41,7 +41,7 @@ blog_title = "misgnros.github.io"
 # keys should be used in ``post`` directive to refer to the locations.
 # Default is ``{}``.
 # blog_locations = {
-#    'Earth': ('The Blue Planet', 'https://en.wikipedia.org/wiki/Earth),
+#    "Earth": ("The Blue Planet", 'https://en.wikipedia.org/wiki/Earth),
 # }
 
 # This will prevent ablog from injecting its own templates into the Sphinx
@@ -52,7 +52,7 @@ blog_title = "misgnros.github.io"
 # -- Blog Post Related --------------------------------------------------------
 
 # Format date for a post.
-# post_date_format = '%%b %%d, %%Y'
+# post_date_format = "%%b %%d, %%Y"
 
 # Number of paragraphs (default is ``1``) that will be displayed as an excerpt
 # from the post. Setting thisi ``0`` will result in displaying no post excerpt
@@ -121,7 +121,7 @@ blog_title = "misgnros.github.io"
 #     "social": {
 #         # Format tags as hashtags and append to the content
 #         "content": "{ title }{% for tag in post.tags %}"
-#         " #{ tag.name|trim()|replace(' ', '') }"
+#         " #{ tag.name|trim()|replace(" ", "") }"
 #         "{% endfor %}",
 #     },
 # }
@@ -167,32 +167,32 @@ blog_title = "misgnros.github.io"
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'misgnros.github.io'
-copyright = '2025, misgnros'
-author = 'misgnros'
+project = "misgnros.github.io"
+copyright = "2025, misgnros"
+author = "misgnros"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
 	"sphinx.ext.githubpages",
-	'sphinx.ext.extlinks',
-	'sphinx.ext.todo',
+	"sphinx.ext.extlinks",
+	"sphinx.ext.todo",
 	"myst_parser",
 	"ablog",
-	'sphinx.ext.intersphinx',
+	"sphinx.ext.intersphinx",
 	"sphinx_design",
 	"sphinxcontrib.mermaid",
 	"sphinx_copybutton",
 	# "sphinxcontrib.googleanalytics",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 source_suffix = {
-	'.rst': 'restructuredtext',
-	'.md': 'markdown',
+	".rst": "restructuredtext",
+	".md": "markdown",
 }
 
 myst_enable_extensions = [
@@ -217,9 +217,9 @@ myst_linkify_fuzzy_links=False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
-html_sidebars = {'**': []}
+html_sidebars = {"**": []}
 
 html_theme_options = {
 	"external_links": [
@@ -241,8 +241,8 @@ html_theme_options = {
 		},
 	],
 	"secondary_sidebar_items": {
-		"**": ["page-toc","sourcelink"],
-		# "index": ["page-toc","sourcelink"],
+		"**": ["page-toc","ablog/tagcloud.html","ablog/categories.html","ablog/archives.html","sourcelink"],
+		"index": ["page-toc","sourcelink"],
 	},
 	"show_prev_next": False,
 	"logo": {
@@ -258,16 +258,16 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+# html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 # html_extra_path = []
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# If not "", a "Last updated on:" timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%%b %%d, %%Y'
+html_last_updated_fmt = "%%b %%d, %%Y"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -298,24 +298,24 @@ html_use_index = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-# html_use_opensearch = ''
+# html_use_opensearch = ""
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 # html_file_suffix = None
 
 # Language to be used for generating the HTML full-text search index.
 # Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-# html_search_language = 'en'
+#   "da", "de", "en", "es", "fi", "fr", "hu", "it", "ja"
+#   "nl", "no", "pt", "ro", "ru", "sv", "tr"
+# html_search_language = "en"
 
 # A dictionary with options for the search language support, empty by default.
-# Now only 'ja' uses this config value
-# html_search_options = {'type': 'default'}
+# Now only "ja" uses this config value
+# html_search_options = {"type": "default"}
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
-# html_search_scorer = 'scorer.js'
+# html_search_scorer = "scorer.js"
 
 # Output file base name for HTML help builder.
 # htmlhelp_basename = "doc"
